7b97ae6a6e7db894c5836ac2d05d3ca58dadbeb4
Hkdel <2282939714@qq.com>
2019-07-27 11:12:32 +0800

    第一次提交中国电信项目

---
+++ README.md
@@ -0,0 +1,1 @@
+China-telecom-online-office
---
+++ js/foot_ad.js
@@ -0,0 +1,5 @@
+var close = document.getElementById("close");
+var foot_ad = document.getElementsByClassName("foot_ad")[0];
+close.onclick = function() {
+	foot_ad.style.display = "none";
+}
